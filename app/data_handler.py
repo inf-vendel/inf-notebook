adb54ebec71f2fc536a86465809ec67e8836782d
inf-vendel <szerk.vendel@gmail.com>
2022-10-17 23:02:34 +0200

    Update server and data-handler to convert and render markdown file as html

--- app/data_handler.py
+++ app/data_handler.py
@@ -1,4 +1,5 @@
 import os
+from markdown import markdown
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 data = os.path.join(basedir, 'data/')
@@ -6,3 +7,15 @@ data = os.path.join(basedir, 'data/')
 def get_items():
     # Return a sorted list of all .md files' names (without extension) as a list
     return sorted([i.replace(".md", "") for i in os.listdir(data) if ".md" in i], reverse=True)
+
+def get_item(page):
+    items = get_items()
+    if page in [p.replace(".md", "") for p in items]:
+        text = ""
+        with open(os.path.join(data, f'{page}.md'), "r", encoding="utf-8") as file:
+            text = file.read()
+        html = markdown(text, extensions=['fenced_code', 'footnotes'])
+        return html
+    else:
+        return f"Not found learning material with {page}."
+    
--- app/server.py
+++ app/server.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from data_handler import get_items
+from data_handler import get_items, get_item
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
+    text = get_item("test_page")
     items = get_items()
-    return f"<h1>It's working!</h1> \n {items[0]}"
+    return f"<h1>It's working!</h1> \n {items[0]} {text}"
 
 
 if __name__ == "__main__":
