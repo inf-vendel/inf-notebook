03b2abd432b972e9821ea429bc82234c5ae8c7ee
inf-vendel <szerk.vendel@gmail.com>
2022-10-18 00:26:26 +0200

    Implement basic js functions

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,29 @@
 let content = document.querySelector(".content");
 (async function getItems() {
-    fetch(`/data`)
-      .then((response) => response.json())
-      .then((data) => setSidebar(data));
-	
+  fetch(`/data`)
+    .then((response) => response.json())
+    .then((data) => setSidebar(data));
 })();
-async function load(page){};
-function setSidebar(data){};
+
+function setContent(page) {
+    content.innerHTML="";
+    content.insertAdjacentHTML("afterbegin", page);
+}
+
+async function load(page) {
+    fetch(`/${page}`)
+    .then((response) => response.json())
+    .then((data) => setContent(data));
+}
+
+function setSidebar(data) {
+  sidebar = document.querySelector(".sidebar-content");
+  sidebar.innerHTML = "<ul>";
+  for (let line of data) {
+    sidebar.insertAdjacentHTML(
+      "afterbegin",
+      `<a href="#" class="nav-link" onclick="load('${line}')">${line}</a>`
+    );
+  }
+  sidebar.insertAdjacentHTML("beforeend", "</ul>");
+}
