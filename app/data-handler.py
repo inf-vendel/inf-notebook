82b713430d1d0834e7d4a51cb57acb232e63c068
inf-vendel <szerk.vendel@gmail.com>
2022-10-17 22:49:57 +0200

    Update server.py and data_handler.py

--- app/data-handler.py
+++
@@ -1,3 +0,0 @@
-def get_items():
-    # Return a sorted list of all .md files' names (without extension) as a list
-    return sorted([i.replace(".md", "") for i in os.listdir(data) if ".md" in i], reverse=True)
--- app/server.py
+++ app/server.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from data_handler import get_items
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return "<h1>It's working!</h2>"
+    items = get_items()
+    return f"<h1>It's working!</h1> \n {items[0]}"
 
 
 if __name__ == "__main__":
