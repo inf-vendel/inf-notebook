8f8bd94a4af25e719e24d0e6cc5ab24fd21ed918
inf-vendel <szerk.vendel@gmail.com>
2022-10-17 22:37:48 +0200

    Add get_items function to data-handler

--- app/data-handler.py
+++ app/data-handler.py
@@ -1,2 +1,3 @@
 def get_items():
-    pass
+    # Return a sorted list of all .md files' names (without extension) as a list
+    return sorted([i.replace(".md", "") for i in os.listdir(data) if ".md" in i], reverse=True)
