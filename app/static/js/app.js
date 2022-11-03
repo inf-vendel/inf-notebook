9f07377fe6f5e58267bda096c122b19fa2bb9b33
inf-vendel <szerk.vendel@gmail.com>
2022-11-03 22:18:59 +0100

    Update delete button, it can be activated on the navbar

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -23,7 +23,7 @@ function setSidebar(data) {
   for (let line of data) {
     sidebar.insertAdjacentHTML(
       "afterbegin",
-      `<div style="display: inline-block"><a href="/remove/${line}"><span class="bi bi-file-x-fill"></span></a><a style="display: inline" href="#" class="nav-link" onclick="load('${line}')">${line}</a> </div>`
+      `<div style="display: inline-block"><a class="delete" href="/remove/${line}"><span class="bi bi-file-x-fill"></span></a><a style="display: inline" href="#" class="nav-link" onclick="load('${line}')">${line}</a> </div>`
     );
   }
   sidebar.insertAdjacentHTML("beforeend", "</ul>");
@@ -36,5 +36,7 @@ function hideInput() {
   input.style.display='none';
 }
 function showDeleteButtons() {
+  let buttons = document.querySelector(".sidebar-content")
+  buttons.classList.remove("hidden")
 
 }
